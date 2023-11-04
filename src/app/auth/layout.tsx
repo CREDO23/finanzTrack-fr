2c53294c1fd5eec4c14629936321e5894c568c24
CREDO23 <thierrybakera12@gmail.com>
2023-11-04 16:01:15 +0200

    feat: add validation on register form

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,19 +1,31 @@
-import { ReactNode } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+
+import { ReactNode, useEffect } from 'react';
 import logo from '../../assets/logo/logo.png';
 import Image from 'next/image';
-import AppLayout from '@/components/layouts/appLayout';
+import { useViewDispatcher } from '@/store/viewState/hooks';
+import { ViewActionType } from '@/store/viewState/action';
 
 export default function Layout({
   children,
 }: {
   children: ReactNode;
 }): JSX.Element {
+  const dispatchView = useViewDispatcher();
+  useEffect(() => {
+    dispatchView({
+      type: ViewActionType.SET_NAVIGATION,
+      payload: false,
+    });
+  }, []);
+
   return (
-      <div className="w-full h-full ">
-        {children}
-        <div className=" absolute bottom-0 right-0 flex items-end justify-end w-auto">
-          <Image height={80} src={logo} alt="logo" />
-        </div>
+    <div className="w-full h-full ">
+      {children}
+      <div className=" absolute bottom-0 right-0 flex items-end justify-end w-auto">
+        <Image height={80} src={logo} alt="logo" />
       </div>
+    </div>
   );
 }
